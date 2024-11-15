ca7576e2b6e9a5a59e91f10959384499e06ae1bc
Jordi-debug <jordifrikigamer@gmail.com>
2024-11-15 19:18:31 +0100

    funcion chips

--- variables.py
+++ variables.py
@@ -1,9 +1,9 @@
-jugadores = {
+players = {
     "player_purple":{
         "color": "purple",
-        "dinero": 100,
+        "money": 100,
         "your_turn":False,
-        "fitxas":{
+        "chips":{
             "fitxa_100":0,
             "fitxa_50":0,
             "fitxa_20":0,
@@ -13,9 +13,9 @@ jugadores = {
     },
     "player_blue":{
         "color": "blue",
-        "dinero": 100,
+        "money": 100,
         "your_turn":False,
-        "fitxas":{
+        "chips":{
             "fitxa_100":0,
             "fitxa_50":0,
             "fitxa_20":0,
@@ -25,9 +25,9 @@ jugadores = {
     },
     "player_orange":{
         "color": "orange",
-        "dinero": 100,
+        "money": 100,
         "your_turn":False,
-        "fitxas":{
+        "chips":{
             "fitxa_100":0,
             "fitxa_50":0,
             "fitxa_20":0,
@@ -36,3 +36,15 @@ jugadores = {
         }
     }
 }
+
+chips = [[1,4,7,10,13,16,19,22,25,28,31,34],
+         [2,5,8,11,14,17,20,23,26,29,32,35],
+         [3,6,9,12,15,18,21,24,27,30,33,36]]
+
+chip_0 = 0
+
+#def lost_money(player) --> maneja la logica de la perdida de diner0
+#def gain_money(player) --> maneja la logica de la ganancia de dinero
+#def manage_money(player) --> en función de si gana o pierde, se añade a player["dinero"]
+#def banca_rota(player)--> maneja la logica de cuando te quedas en banca rota
+mou
