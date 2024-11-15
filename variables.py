95b1dbdfdcaf4a57ff415873af699522e61ab4dc
Jordi-debug <jordifrikigamer@gmail.com>
2024-11-15 18:33:26 +0100

    Var. Players added

---
+++ variables.py
@@ -0,0 +1,38 @@
+jugadores = {
+    "player_purple":{
+        "color": "purple",
+        "dinero": 100,
+        "your_turn":False,
+        "fitxas":{
+            "fitxa_100":0,
+            "fitxa_50":0,
+            "fitxa_20":0,
+            "fitxa_10":0,
+            "fitxa_5":0
+        }
+    },
+    "player_blue":{
+        "color": "blue",
+        "dinero": 100,
+        "your_turn":False,
+        "fitxas":{
+            "fitxa_100":0,
+            "fitxa_50":0,
+            "fitxa_20":0,
+            "fitxa_10":0,
+            "fitxa_5":0
+        }
+    },
+    "player_orange":{
+        "color": "orange",
+        "dinero": 100,
+        "your_turn":False,
+        "fitxas":{
+            "fitxa_100":0,
+            "fitxa_50":0,
+            "fitxa_20":0,
+            "fitxa_10":0,
+            "fitxa_5":0
+        }
+    }
+}
