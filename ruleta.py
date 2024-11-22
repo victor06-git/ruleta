bee7ef3e8d1ea11ce94343a7f7387cc6b288bb61
victor06-git <asensiovector@gmail.com>
2024-11-22 22:59:53 +0100

    #

--- ruleta.py
+++ ruleta.py
@@ -20,6 +20,7 @@ DARK_GREEN = (0, 180, 0)
 GREY = (120,120,120)
 YELLOW = (243,228,67)
 BROWN = (98,52,18)
+GRAY = (128,128,128)
 
 
 
@@ -41,11 +42,17 @@ pygame.display.set_caption('Ruleta')
 clicked = False
 mouse_x, mouse_y = -1, -1
 
+rad_first = ((360 / 37) * (math.pi / 180)) #First angle
+rad_second = ((360 / 37) * (math.pi / 180) + (rad_first)) #Second angle
+
+rad_1 = rad_second
+rad_2 = ((360 / 37) * (math.pi / 180) + (rad_1))
+
+rad_num = ((360 / 37) * (math.pi / 180) * 5) / 2
+rad_num1 = ((360 / 37) * (math.pi / 180) * 3) / 2
 
 
-
-
-
+change = False
 
 # Bucle de l'aplicació
 def main():
@@ -65,10 +72,11 @@ def main():
 # Gestionar events
 # Gestionar events
 def app_events():
-    global clicked
+    global clicked, rotation, change
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Botón cerrar ventana
+            rotation = False
             return False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             clicked = True
@@ -78,8 +86,12 @@ def app_events():
 
 # Fer càlculs
 def app_run():
+    global rad_first, rad_second
     mouse_x, mouse_y = pygame.mouse.get_pos()
     pass
+
+    
+    
         
 # Dibuixar
 def app_draw():
@@ -89,14 +101,17 @@ def app_draw():
 
     draw_grid()
     
-    draw_roulette()
-    table()
+    draw_roulette() #Función draw ruleta
+    table() #Función dibujar tabla
     
     pygame.display.update()
 
+
+
 #Roulette
 def draw_roulette():
-
+    global rad_first, rad_second
+    
     rad_first = ((360 / 37) * (math.pi / 180)) #First angle
     rad_second = ((360 / 37) * (math.pi / 180) + (rad_first)) #Second angle
 
@@ -106,6 +121,7 @@ def draw_roulette():
     rad_num = ((360 / 37) * (math.pi / 180) * 5) / 2
     rad_num1 = ((360 / 37) * (math.pi / 180) * 3) / 2
    
+    
     #Ruleta parte verde del 0
     pygame.draw.polygon(screen, GREEN, [((screen_width // 2) / 2, screen_height // 2 - 100),
                         ((screen_width // 2) / 2 + (250 * math.cos(rad_first)), screen_height // 2 + (250 * math.sin(rad_first)) - 100),
@@ -130,14 +146,43 @@ def draw_roulette():
                                              (screen_width // 2 / 2 + (250 * math.cos(rad_2)), screen_height // 2 + (250 * math.sin(rad_2)) - 100)], 2)
         rad_1 += ((360 / 37) * (math.pi / 180))
         rad_2 += ((360 / 37) * (math.pi / 180))
-    #Centro ruleta y borde
+    
+    #Centro ruleta y borde / decoración
     pygame.draw.circle(screen, BROWN, (screen_width // 2 / 2, screen_height // 2 - 100), 260, 20)
     pygame.draw.circle(screen, BROWN, (screen_width // 2 / 2, screen_height // 2 - 100), 150)
+    pygame.draw.circle(screen, GRAY, (screen_width // 2 / 2, screen_height // 2 - 100), 190, 10)
+    
+    #Lineas interiors ruleta
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2, screen_height // 2 + 50), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2, screen_height // 2 - 250), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 + 148, screen_height // 2 - 100), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 - 148, screen_height // 2 - 100), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 + (148 * math.cos(math.pi / 4)), screen_height // 2 - 100 + (148 * math.sin(math.pi / 4))), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 - (148 * math.cos(math.pi / 4)), screen_height // 2 - 100 + (148 * math.sin(math.pi / 4))), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 + (148 * math.cos(1.25 * math.pi)), screen_height // 2 - 100 + (148 * math.sin(1.25 * math.pi))), 3)
+    pygame.draw.line(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 - (148 * math.cos(1.25 * math.pi)), screen_height // 2 - 100 + (148 * math.sin(1.25 * math.pi))), 3)
+    
+    #Part interior
+    pygame.draw.circle(screen, BLACK, (screen_width // 2 / 2, screen_height // 2 - 100), 30)
+    pygame.draw.circle(screen, YELLOW, (screen_width // 2 / 2, screen_height // 2 - 100), 18)
+    pygame.draw.line(screen, YELLOW, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2, screen_height // 2 - 30), 5)
+    pygame.draw.circle(screen, YELLOW, (screen_width // 2 /2, screen_height // 2 - 30), 10)
+    
+    pygame.draw.line(screen, YELLOW, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2, screen_height // 2 - 170), 5)
+    pygame.draw.circle(screen, YELLOW, (screen_width // 2 /2, screen_height // 2 - 170), 10)
+   
+    pygame.draw.line(screen, YELLOW, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 + 70, screen_height // 2 - 100), 5)
+    pygame.draw.circle(screen, YELLOW, (screen_width // 2 / 2 + 70, screen_height // 2 - 100), 10)
 
+    pygame.draw.line(screen, YELLOW, (screen_width // 2 / 2, screen_height // 2 - 100), (screen_width // 2 / 2 - 70, screen_height // 2 - 100), 5)
+    pygame.draw.circle(screen, YELLOW, (screen_width // 2 / 2 - 70, screen_height // 2 - 100), 10)
+    
     #lista números ruleta
-    roulette_numbers = [32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 
-                        11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 
-                        9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
+    roulette_numbers = [32, 15, 19, 4, 21, 2, 25,
+                        17, 34, 6, 27, 13, 36, 11,
+                        30, 8, 23, 10, 5, 24, 16, 
+                        33, 1, 20, 14, 31, 9, 22, 
+                        18, 29, 7, 28, 12, 35, 3, 26]
     
     x0 = (screen_width // 2 / 2 ) + (215 * math.cos(rad_num1))
     y0 = screen_height // 2  + (215 * math.sin(rad_num1)) - 100
@@ -157,65 +202,99 @@ def draw_roulette():
 
 #Tabla de apuestas
 def table():
+    #Parte del 0
     points_rect0 = [(1100, 50), (950, 100), (1250, 100), (1100, 50)]
     pygame.draw.polygon(screen, BLACK, points_rect0, 3)
+    pygame.draw.polygon(screen, DARK_GREEN, points_rect0)
     
 
-    numeros_caselles = {
-        "Primera":{
-            "Rojo": [3, 9, 18, 21, 27, 36],
-            "Negro": [6, 12, 15, 24, 30, 33]
-        },
-        "Segunda": {
-            "Rojo": [5,11,17 , 23,29, 32],
-            "Negro": [2, 8, 14, 20, 26, 35]
-        },
-        "Tercero": {
-            "Rojo": [1, 7, 13, 19, 25, 31],
-            "Negro": [4, 10, 16, 22, 28, 34]
-        }
-    }
-
+    font = pygame.font.SysFont(None, 30)
+    text = font.render(str(0), True, WHITE)
+    text_rect = (((950 + 1250) / 2 - 4) , 70) #Posicion de texto 0 centrado
+    screen.blit(text, text_rect)
+    
     height_casella = (600 / 13)
-    width_casella = (300 / 3)     
+    width_casella = (300 / 3)
+
+    #Casillas apuesta columna
+    pygame.draw.rect(screen, DARK_GREEN, (950 + (width_casella), 100 + (600 / 13) * 12, width_casella, height_casella))
+    pygame.draw.rect(screen, DARK_GREEN, (950 - 3 , 100 + (600 / 13) * 12, width_casella + 3, height_casella))
+    pygame.draw.rect(screen, DARK_GREEN, (950 + (2 * width_casella), 100 + (600 / 13) * 12, width_casella, height_casella))
+    pygame.draw.rect(screen, BLACK, (950 + (width_casella), 100 + (600 / 13) * 12, width_casella, height_casella), 3)
+    pygame.draw.rect(screen, BLACK, (950 - 3 , 100 + (600 / 13) * 12, width_casella + 3, height_casella), 3)
+    pygame.draw.rect(screen, BLACK, (950 + (2 * width_casella), 100 + (600 / 13) * 12, width_casella, height_casella), 3)
+    font = pygame.font.SysFont(None, 60)
+    text = font.render(str("2 to 1"), True, WHITE)
+    text_rect = (((950 + 1250) / 2 - 4) , 70) #Posicion de texto 0 centrado
+    screen.blit(text, text_rect)
+    
+    #Pares/ Impares/ Rojo/ Negro
+    pygame.draw.rect(screen, DARK_GREEN, (850, 100 ,100, 100))
+    pygame.draw.rect(screen, DARK_GREEN, (850 , 200, 100, 180))
+    pygame.draw.rect(screen, DARK_GREEN, (850 , 380, 100, 180))
+    pygame.draw.rect(screen, DARK_GREEN, (850 ,555, 100, 100))
+    pygame.draw.rect(screen, BLACK, (850, 100 ,100, 100), 3)
+    pygame.draw.rect(screen, BLACK, (850 , 200, 100, 180), 3)
+    pygame.draw.rect(screen, BLACK, (850 , 380, 100, 180), 3)
+    pygame.draw.rect(screen, BLACK, (850 ,555, 100, 100), 3)
+
+    #Rombos rojo y negro
+    pygame.draw.polygon(screen, RED, [(900, (200 + ((180 / 2) / 4))),
+                                      (850 + (100 / 4), (200 + (180 / 2))), 
+                                      (900, (200 + 3 * (180 / 4) + 25)),
+                                      (950 - ((100 / 4)), (200 + (180 / 2)))])
+    
+    pygame.draw.polygon(screen, BLACK, [(900, (380 + ((180 / 2) / 4))),
+                                      (850 + (100 / 4), (380 + (180 / 2))), 
+                                      (900, (380 + 3 * (180 / 4) + 25)),
+                                      (950 - ((100 / 4)), (380 + (180 / 2)))])
+    
+
+
+
     for  columna in range(3):
         for fila in range(12):
             
-            if columna == 0 or columna == 2:
-                if fila % 2 == 0:
+            if columna == 0:
+                if fila == 0 or  fila ==2 or  fila ==5 or fila == 6 or  fila ==8 or fila ==11:
                     color = RED
                 else: 
                     color = BLACK
             if columna == 1:
-                if fila % 2 == 0:
+                if fila == 0 or fila == 2 or fila ==3 or  fila ==5 or fila ==6 or fila ==8 or fila == 9 or fila ==11:
                     color = BLACK
                 else:
                     color = RED
-           
-            if color == RED:
-                color_number = "Rojo"
-            else:
-                color_number = "Negro"
-            
-            if columna == 0:
-                column_number = "Tercero"
-            elif columna == 1:
-                column_number = "Segunda"
-            else:
-                column_number = "Primera"
+            if columna == 2:
+                if fila == 0 or fila == 2 or fila ==3 or fila ==5 or  fila ==6 or  fila ==8 or  fila ==9 or  fila ==11:
+                    color = RED
+                else:
+                    color = BLACK
 
+            if columna == 0:
+                column_number = 0
+            elif columna == 1:
+                column_number = 1
+            else:
+                column_number = 2
             
-            pygame.draw.rect(screen, WHITE, (950 + (columna * width_casella), 100 + (fila * height_casella), width_casella, height_casella), 3) #Contorno casilla
+
+            #Casillas apuestas números
+            pygame.draw.rect(screen, BLACK, (950 + (columna * width_casella), 100 + (fila * height_casella), width_casella, height_casella), 3) #Contorno casilla
             pygame.draw.rect(screen, color, (950 + (columna * width_casella), 100 + (fila * height_casella), width_casella, height_casella)) #Relleno de color la casilla
             
             #Números casillas
-            numbers = numeros_caselles[column_number][color_number][fila // 2]
+            numbers = chips[column_number][fila]
             font = pygame.font.SysFont(None, 25)
             text = font.render(str(numbers), True, WHITE)
             text_rect = (950 + (columna * width_casella) + 50, 100 + ( fila * height_casella) + 15) #Posicion de texto
             screen.blit(text, text_rect)
+    
 
+
+#graellas
 def draw_grid():
+    
     # Color de la cuadrícula
     GRID_COLOR = (50, 50, 50)
     
@@ -238,9 +317,7 @@ def draw_grid():
         # Añadir texto de coordenadas Y
         font = pygame.font.Font(None, 20)
         text = font.render(str(y), True, (200, 200, 200))
-        screen.blit(text, (2, y+2))
-
-
+        screen.blit(text, (2, y + 2))
 
 if __name__ == "__main__":
     main()
